a62f2ec1d1b070fd138d2572564615c1ff2e2920
AlexMannDesigns <alex.mann1989@gmail.com>
2022-01-22 21:06:53 +0200

    figured out how to access the necessary JSON data returned by the API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
+import React, {useEffect, useState} from "react";
+
 function App() {
-  return (
-    <div>
-      <header>
-        <p>Hello world! Isn't react fun?</p>
-      </header>
-    </div>
-  );
+
+	const [songs, setSongs] = useState(0);
+	//const [query, setQuery] = useState("default");
+
+	//populate app with top 10 songs by default
+	useEffect(() => {
+		getSongs();
+	});
+
+	async function getSongs() {
+		const response = await fetch(`https://itunes.apple.com/us/rss/topsongs/limit=100/json`);
+		const data = await response.json();
+		console.log(data);
+		for (let i = 0 ; i < 30 ; i++) {
+			console.log("artist: " + data.feed.entry[i]['im:artist'].label);
+			console.log("title: " + data.feed.entry[i].title.label);
+			console.log("price: " + data.feed.entry[i]['im:price'].label);
+			console.log("img url: " + data.feed.entry[i]['im:image'][2].label);
+		}
+	}
+
+	return (
+		<div>
+			<header>
+				<p>you clicked {songs} times! Isn't react fun?</p>
+				<button onClick={() => setSongs(songs + 1)}>click moi</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
