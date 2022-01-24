6492173b6940ad2f7194025b0e14779c296d357e
AlexMannDesigns <alex.mann1989@gmail.com>
2022-01-24 19:40:05 +0200

    layout and design updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import Song from "./Song.jsx";
 const App = () => {
 	const [songs, setSongs] = useState([]);
 	const [type, setType] = useState(false);
+	const [search, setSearch] = useState("");
+	const [query, setQuery] = useState("");
 
 	//populate app with top 30 songs by default. Re-render results if 'type' changes
+	//useEffect's 2nd parameter should stop repetitive calls to API, and monitors changes to type and search
 	useEffect(() => {
 		getSongs();
-	}, [type]);
+	}, [type, query]); // eslint-disable-line react-hooks/exhaustive-deps
 
 	async function getSongs() {
 		const loadingMessage = document.querySelector(".loading");
 		const errorMessage = document.querySelector(".error");
 		const songsOrAlbums = type ? "albums" : "songs";
-		console.log("test to see if this only runs once"); //useEffect's second parameter should stop repetitive calls
 		fetch(`https://itunes.apple.com/us/rss/top${songsOrAlbums}/limit=100/json`)
 			.then(async response => {
 				const data = await response.json();
@@ -24,8 +26,13 @@ const App = () => {
 				if (response.ok) {
 					//pus JSON data into an array so it can be mapped as a component
 					let songArr = [];
-					for (let i = 0 ; i < 30 ; i++)
-						songArr.push(data.feed.entry[i]);
+					for (let i = 0 ; i < 30 ; i++) {
+						if (query.length) { //clicking search with an empty search bar returns default results
+							if (data.feed.entry[i]['im:artist'].label.includes(query)) //check if artist name contains search term (case sensitive)
+								songArr.push(data.feed.entry[i]);
+						} else
+							songArr.push(data.feed.entry[i]);
+					}
 					setSongs(songArr);
 					return ;
 				}
@@ -44,25 +51,46 @@ const App = () => {
 	}
 
 	function changeResults(e) {
-		setType(e.target.checked);
-		//useEffect will re-render our results due to its second parameter changing
+		setType(e.target.checked); //useEffect will re-render our results due to its second parameter changing
+	}
+
+	function getSearch(e) {
+		e.preventDefault();
+		setQuery(search); //sets query to inputted text (see updateSearch function)
+		setSearch(""); //resets search bar
+	}
+
+	function updateSearch(e) {
+		setSearch(e.target.value); //updates search variable with text from searchbar
 	}
 
 	return (
 		<div>
 			<header>
-				<input type="checkbox" id="selector" className="toggle" onChange={changeResults}></input>
-    			<label for="selector" className="label">
-					<div className="ball"></div>
-				</label>
-      			<span>Albums</span>
+				<form className="search-form" onSubmit={getSearch}>
+					<div className="toggle-container">
+						<span>Singles</span>
+						<input type="checkbox" id="selector" className="toggle" onChange={changeResults}/>
+    					<label for="selector" className="label">
+							<div className="ball"></div>
+						</label>
+      					<span>Albums</span>
+					</div>
+					<div>
+						<h1>iTunes Top 30</h1>
+					</div>
+					<div>
+						<input className="search-bar" type="text" onChange={updateSearch} value={search} placeholder="Search artists (case-sensitive)..." />
+						<button className="search-button" type="submit">Search</button>
+					</div>
+				</form>
 			</header>
 			<p className="loading">LOADING...</p>
 			<div className="error">
 				<p>ERROR</p>
 				<button onClick={() => getSongs}>RETRY</button>
 			</div>
-			<div>
+			<div className="main">
 				{songs.map((song, idx) => (
 					<Song
 					key={idx}
--- src/Song.jsx
+++ src/Song.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
 const Song = (props) => {
+
+	function truncateString(str) {
+		if (str.length > 30)
+			return str.slice(0, 30) + "...";
+		return str;
+	}
+
+	let title = truncateString(props.title.substring(0, props.title.indexOf('-') - 1));
+	let artist = truncateString(props.artist);
+
 	return (
-		<div>
-		<h2>{props.title}</h2>
-		<p><em>by {props.artist}</em></p>
+		<div className="music-container">
+		<h2>{title}</h2>
+		<p><em>by {artist}</em></p>
 		<p><strong>{props.price}</strong></p>
 		<img src={props.imageURL} alt="cover art"></img>
 		</div>
